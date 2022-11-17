65baf557dab772ae9b1172613b0dc366dd81d1c2
dbezugliy <dbezugliy@imsa.edu>
2022-11-17 16:43:14 -0600

    change

--- pagerank.cpp
+++ pagerank.cpp
@@ -1,8 +1,22 @@
 #include "pagerank.h"
 
 void PageRank::integrate(std::vector<std::vector<std::string>> csv) {
-    for () {
+    for (std::vector<std::string> edge : csv) {
+        edges[edge[0]].push_back(edge[1]);
+        edges[edge[1]].push_back(edge[0]);
+        // std::string node1 = edge[0];
+        // std::string node2 = edge[1];
+        // if (edges.find(node1) != edges.end()) {
+        //     edges[node1].push_back(node2);
+        // } else 
         
+        // if (edges.find(node2) != edges.end()) {
+        //     edges[node2].push_back(node1);
+        // }
+    }
+    for (auto const& x : edges) {
+        original[x.first] = 1 / edges.size();
+        modified[x.first] = 1 / edges.size();
     }
 }
 
