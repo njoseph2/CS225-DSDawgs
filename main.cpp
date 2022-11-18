8c39bc084974c7dbe80cb7eed5e20e6faac3ce71
fshaik032 <fshaik032@gmail.com>
2022-11-18 08:26:21 -0800

    main

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
+#include "CSV.h"
+#include "pagerank.h"
 
 int main() {
-    //test something!
+    std::vector<std::vector<std::string>> file;
+    //file = readCSV("testing.csv");
+    file = readCSV("large_twitch_edges.csv");
+    //std::cout << file.size() << std::endl;
+
+    PageRank run;
+    run.integrate(file);
+    run.algorithm();
+    std::cout << run.expressRanks() << std::endl;
     return 0;
 };
